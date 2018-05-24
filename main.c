6cb81bce25f661756511fa8473930b73eef9408c
ProjectSol <scobiesolomon@gmail.com>
2018-05-24 13:08:42 +1000

    WE've done some good work so far
    Just gotta keep going.

--- main.c
+++ main.c
@@ -18,6 +18,7 @@
 struct Team {
   char name[16];
   char group[2];
+  int grAsNum;
   int gPlayed;
   int gWon;
   int gLost;
@@ -31,7 +32,10 @@ struct Team newTeamGeneration();
 void getTeamList(struct Team *list);
 void printNameGroup(struct Team *teamList);
 void pullMatchStats(struct Team *list);
-void mainLoop(struct Team *list);
+void outputTeamStandings(struct Team *list);
+void sortByGroup(struct Team *list);
+void bubbleSortByGroups(struct Team *list, const int arraySize);
+
 
 
 int main(void)
@@ -39,19 +43,76 @@ int main(void)
   //All the data we will need for sure is pulled and stored into our necessary array here
   struct Team teamList[NUM_TEAMS];
   getTeamList(teamList);
-  printNameGroup(teamList);
   pullMatchStats(teamList);
+  sortByGroup(teamList);
   //Now that we have things defined we can start up our main loop and
   //let the user define which functions of the program they would lke to use
-  mainLoop(teamList);
+  outputTeamStandings(teamList);
+}
+
+void sortByGroup(struct Team *list)
+{
+  for (int i = 0; i<NUM_TEAMS; i++)
+  {
+    char ch = list[i].group[0];
+    int pos = ch - 'A' + 1;
+    list[i].grAsNum = pos;
+    printf("%s %d\n",list[i].name,list[i].grAsNum);
+  }
+  bubbleSortByGroups(list, NUM_TEAMS);
+}
+
+void bubbleSortByGroups(struct Team *list, const int arraySize)
+{
+int	i,	k;
+  for(i	=	0;	i	<	arraySize;	i++)
+  {
+    for(k	=	0;	k	<	arraySize-1;	k++)
+    {
+      if(abs(list[k].grAsNum) > abs(list[k+1].grAsNum))
+      {
+        printf("Swapping entries %d and %d",k,i);
+        struct Team temp = list[k];
+        list[k] = list[i];
+        list[i] = temp;
+      }
+    }
+  }
+}
+
+void outputTeamStandings(struct Team *list)
+{
+  char initialGroup[2] = "-";
+  //Split for neatness and also doing this for some reason fixed a bug I was having so...
+  printf("| Group | %-16s | %-13s | %-13s | %-13s ","Name","Games Played", "Games Won","Games Drawn");
+  printf("| %-13s | %-13s | %-13s | %-13s |\n","Games Lost","Goals For", "Goals Against","Points");
+  for (int i = 0; i<NUM_TEAMS; i++)
+  {
+    list[i].points = list[i].gWon*3+list[i].gDrawn;
+    struct Team t = list[i];
+    if (strcmp(initialGroup,list[i].group)!=0)
+    {
+      strcpy(initialGroup, list[i].group);
+      //Split for neatness and also doing this for some reason fixed a bug I was having so...
+      printf("\n| Group | %-16s | %-13s | %-13s | %-13s ","Name","Games Played", "Games Won","Games Drawn");
+      printf("| %-13s | %-13s | %-13s | %-13s |\n","Games Lost","Goals For", "Goals Against","Points");
+
+        printf("| %-5s | %-16s | %-13d | %-13d | %-13d | %-13d | %-13d | %-13d | %-13d |\n",t.group,t.name,t.gPlayed,t.gWon,t.gDrawn,t.gLost,t.goalFor,t.goalAgainst,t.points);
+    }
+    else
+    {
+      printf("| %-5s | %-16s | %-13d | %-13d | %-13d | %-13d | %-13d | %-13d | %-13d |\n","",t.name,t.gPlayed,t.gWon,t.gDrawn,t.gLost,t.goalFor,t.goalAgainst,t.points);
+    }
+  }
 }
 
 void printNameGroup(struct Team *teamList)
 {
   for (int i = 0; i<NUM_TEAMS; i++)
   {
-    printf("%s ",teamList[i].name);
+    printf("%s\n",teamList[i].name);
     printf("%s\n",teamList[i].group);
+    printf("%d\n",teamList[i].goalFor);
   }
 }
 
@@ -71,85 +132,109 @@ void pullMatchStats(struct Team *list)
   //be equal to zero. If it is +1 we breakout and run the next check still
   //resetting to zero
   int breakOut = 0;
-
+  int counter = 0;
   while(loop == 1)
   {
+
     // Reading the file and storing what we need
     if ( fscanf(matchList, "%s %d %s %d", tName1, &score1, tName2, &score2) == EOF )
     {
+      printf("End of file has been reached after %d loops\n");
       loop = -1;
     }
+    counter ++;
 
+    /*
+     * Alright so this was unnecessarily complex but it works just fine, we've got three nested loops
+     * and they're all essential to this method working.
+     * The first loop is based on the variable loop, original I know, that is merely
+     * a check to ensure that we did not reach the end of file before entering into
+     * our checks and editing of the team win loss data. The next iterates through twice so
+     * that we check both the first and second teams setting our current score and current name
+     * accordingly. The third is what acutally loops through each of the existing team names to
+     * check that the teams being input actually exist. Towards the end we have a breakout routine
+     * all this does is check if we've reached the end of the existing teams or if we found the
+     * correct team then moves on to check whether we should update the data and if we should it does.
+     */
     if (loop == 1)
     {
-      for (int i = 0; i<NUM_TEAMS;i++)
+      for (int k = 0; k<2; k++)
       {
-        int compare1 = strcmp(list[i].name, tName1);
-        if (compare1 == 0)
+        char currName[16], altName[16];
+        int currScore, altScore;
+
+        if (k == 0)
         {
-          if(score1>=0)
+          currScore = score1;
+          strcpy(currName, tName1);
+          altScore = score2;
+          strcpy(altName, tName2);
+        }
+        else
+        {
+          currScore = score2;
+          strcpy(currName, tName2);
+          altScore = score1;
+          strcpy(altName, tName1);
+        }
+
+        //printf("%s\n", currName);
+        for (int i = 0; i<NUM_TEAMS;i++)
+        {
+          int compare1 = strcmp(list[i].name, currName);
+          if (compare1 == 0)
           {
-            printf("score1: %d name: %s\n", score1, tName1);
-            teamNum1 = i;
-            breakOut == 1;
+            if(currScore>=0)
+            {
+              //printf("%d %s %s\n",i,list[i].name,currName);
+              //printf("Great Success!! score: %d name: %s\n", currScore, currName);
+              if (k == 0)
+              {
+                teamNum1 = i;
+              }
+              else
+              {
+                teamNum2 = i;
+              }
+              breakOut = 1;
+            }
+            else
+            {
+              //printf("%d %s %s\n",i,list[i].name,currName);
+              printf("%s %d %s %d - scores may not be negative, %s\n", tName1, score1, tName2, score2, currName);
+              breakOut = -1;
+            }
           }
           else
           {
-            printf("%s %d %s %d - scores may not be negative, %d\n", tName1, score1, tName2, score2, score1);
-            breakOut = -1;
+            //printf("%d %s %s\n",i,list[i].name,currName);
+            if (i == NUM_TEAMS-1)
+            {
+              printf("%s %d %s %d - The team name %s does not exist\n", tName1, score1, tName2, score2, currName);
+              breakOut = -1;
+            }
           }
-        }
-        else
-        {
-          if (i = NUM_TEAMS-1)
+          if (breakOut == 1)
           {
-            printf("%d %s %s\n",i,list[i].name,tName1);
-            printf("%s %d %s %d - The team name %s does not exist\n", tName1, score1, tName2, score2, tName1);
-            breakOut = -1;
+            //printf("running breakout 1\n");
+            breakOut = 0;
+            break;
+          }
+          else if (breakOut == -1)
+          {
+            //printf("running breakout -1\n");
+            break;
           }
         }
-        if (breakOut == 1)
+        if (breakOut == -1)
         {
-          printf("running breakout 1\n");
-          breakOut == 0;
-          break;
-        }
-        else
-        {
-          printf("running breakout 2\n");
-          breakOut = -1;
+          //  printf("breakout -1 part 2");
           break;
         }
       }
-    }
-
-    /*for (int i = 0; i<NUM_TEAMS;i++)
-    {
-      int compare2 = strcmp(list[i].name, tName2);
-      if (compare2 == 0)
-      {
-        if(score1>=0)
-        {
-          printf("score1: %d name: %s\n", score2, tName2);
-          teamNum1 = i;
-          breakOut == 1;
-        }
-        else
-        {
-          printf("%s %d %s %d - scores may not be negative, %d\n", tName1, score1, tName2, score2, score2);
-          breakOut = -1;
-        }
-      }
-      else
-      {
-        if (i = NUM_TEAMS-1)
-        {
-          printf("%d %s %s",i,list[i].name,tName2);
-          printf("%s %d %s %d - The team name %s does not exist\n", tName1, score1, tName2, score2, tName2);
-          breakOut = -1;
-        }
-      }
-      if (breakOut == 1)
+      //Once we're out of the first for loop we're checking to see if we should
+      //update the data or not and if yes we do that thing.
+      if (breakOut != -1)
       {
         list[teamNum1].goalFor += score1;
         list[teamNum2].goalFor += score2;
@@ -158,21 +243,28 @@ void pullMatchStats(struct Team *list)
         if (score1>score2)
         {
           list[teamNum1].gWon ++;
-          list[teamNum1].gLost ++;
+          list[teamNum2].gLost ++;
         }
-        else
+        else if (score2>score1)
         {
           list[teamNum2].gWon ++;
           list[teamNum1].gLost ++;
         }
+        else
+        {
+          list[teamNum2].gDrawn ++;
+          list[teamNum1].gDrawn ++;
+        }
+        //printf("Values were changed for %s and %s\n", tName1, tName2);
       }
       else
       {
-        breakOut = -1;
-        break;
+        //printf("No values were changed for %s and %s\n", tName1, tName2);
+        breakOut = 0;
       }
-    }*/
+    }
   }
+  fclose(matchList);
 }
 
 void getTeamList(struct Team *list)
@@ -207,11 +299,6 @@ void getTeamList(struct Team *list)
     }
   } while(loop != -1);
   fclose(teamList);
-}
-
-void mainLoop(struct Team *list)
-{
-  printf("Now you should put in an acutal main loop hey");
 }
 
 struct Team newTeamGeneration()
